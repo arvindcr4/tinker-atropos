7a644b8978cb2aa91ebe0a3e33d4ce3dc29cb3f3
Sam Herring <samherring99@gmail.com>
2025-11-05 15:34:38 -0800

    Added print outputs for weight downloads

--- tinker_atropos/utils/download_weights.py
+++ tinker_atropos/utils/download_weights.py
@@ -7,9 +7,13 @@ TINKER_PATH = "tinker://<unique_id>/sampler_weights/final"
 
 OUTPUT_FILENAME = "archive.tar"
 
+print("Downloading weights from Tinker...")
+
 sc = tinker.ServiceClient()
 rc = sc.create_rest_client()
 future = rc.get_checkpoint_archive_url_from_tinker_path(TINKER_PATH)
 checkpoint_archive_url_response = future.result()
 
 urllib.request.urlretrieve(checkpoint_archive_url_response.url, OUTPUT_FILENAME)
+
+print(f"Finished! Weights are available at {OUTPUT_FILENAME}")
